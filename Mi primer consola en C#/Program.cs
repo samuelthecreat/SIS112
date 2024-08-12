af14e915b0f31d86f9baab3938190379f81af0f8
samuelthecreat <samuel.aguilera@ucb.edu.bo>
2024-08-12 11:59:44 -0400

    C#

--- Mi primer consola en C#/Program.cs
+++ Mi primer consola en C#/Program.cs
@@ -8,21 +8,41 @@ class Program
         //HelloWorld()
 
         //segundo metodo, mostrar la edad ingresada por el usuario
+        Obtainage();
+
+        //tercer metodo, calcular año de nacimiento
+        AgeCalculation();
 }
 
-public static void ObtenerEdad()
+public static void Obtainage()
     {
-        Console.WriteLine("Introduzca su edad");
-        string? edadInput = Console.ReadLine();
-        if(string.IsNullOrEmpty(edadInput))
+        Console.WriteLine("Introduce your age");
+        string? ageInput = Console.ReadLine();
+        if(string.IsNullOrEmpty(ageInput))
         {
-            Console.WriteLine("introduzca un valor");
+            Console.WriteLine("introduce a value");
         }
         else
         {
-            int edadinteger = Int32.Parse(edadInput);
-            Console.WriteLine("Tu edad es: " + edadinteger);
+            int ageinteger = Int32.Parse(ageInput);
+            Console.WriteLine("Your age is: " + ageinteger);
         }
 
+    }
+    
+    public static void AgeCalculation()
+    {
+        Console.WriteLine ("introduce the year you were born");
+        string? boryearInput = Console.ReadLine();
+        if (string.IsNullOrEmpty(boryearInput))
+        {
+            Console.WriteLine("Introduce a correct value");
+        }
+            else
+        {
+            int Bornyear = Int32.Parse(boryearInput);
+            Console.WriteLine("Your age is: " + (2024 - Bornyear));
+        }
+        
     }
 }
--- Mi primer consola en C#/obj/Debug/net8.0/Mi primer consola en C#.AssemblyInfo.cs
+++ Mi primer consola en C#/obj/Debug/net8.0/Mi primer consola en C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mi primer consola en C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a84ca4b98611fe7746d72950ddb7e05373f4fb8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f0c526296ade8f1f2ce527fabe86d8634e5685e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mi primer consola en C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mi primer consola en C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
