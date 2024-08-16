ae27ab5c68e2baa96f87989703671196bcdbd2d9
samuelthecreat <samuel.aguilera@ucb.edu.bo>
2024-08-16 10:43:34 -0400

    Create #Algoritmos.py

---
+++ python/#Algoritmos.py
@@ -0,0 +1,19 @@
+#Algoritmos
+
+#Obtener edad
+def Hola_mundo():
+    print("Hello World")
+
+def Obtener_edad():
+    edad = input("inserte su edad")
+    print ("Su edad es: ", edad)
+
+
+def Calcular_edad():
+    anio = int(input(" inserte el año de nacimiento"))
+    anio = 2024 - anio
+    print (anio)
+
+Hola_mundo()
+Obtener_edad()
+Calcular_edad()
