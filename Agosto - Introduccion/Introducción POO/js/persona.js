c324c0f0634831e8903ac5675afd0696d8f50710
samuelthecreat <samuel.aguilera@ucb.edu.bo>
2024-08-23 11:38:32 -0400

    js

--- Agosto - Introduccion/Introducción POO/js/persona.js
+++ Agosto - Introduccion/Introducción POO/js/persona.js
@@ -1,3 +1,29 @@
-class Persona{
-    
-}
+class Persona {
+    constructor(nombre, edad, carrera) {
+      this.nombre = nombre;
+      this.edad = edad;
+      this.carrera = carrera;
+    }
+  
+    saludar() {
+      console.log('Hola, mi nombre es ' + this.nombre + ' y tengo ' + this.edad +' años.');//print
+    }
+  
+    cumpleaños() {
+      this.edad = this.edad + 1;
+      console.log('¡Feliz cumpleaños! Ahora tengo ' + this.edad + ' años.');
+    }
+  
+    estudiar() {
+      console.log('Estoy estudiando ' + this.carrera);
+    }
+  }
+
+  function SaludoPersona1(){
+    // Crear una instancia de la clase Persona
+    //declara la variable usando: var / const / let    
+    const persona1 = new Persona("Ana", 22, "Ingeniería Informática"); 
+    // Llamar a los métodos
+    persona1.saludar();
+  }
+  
