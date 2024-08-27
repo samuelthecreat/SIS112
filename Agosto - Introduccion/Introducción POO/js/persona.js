c72dc5ceb08c3df690cf73beca4b691ceddd2284
samuelthecreat <samuel.aguilera@ucb.edu.bo>
2024-08-27 18:03:08 -0400

    js

--- Agosto - Introduccion/Introducción POO/js/persona.js
+++ Agosto - Introduccion/Introducción POO/js/persona.js
@@ -6,6 +6,7 @@ class Persona {
     this.universidad = universidad;
   }
 
+  //estas funciones son las bases
   saludar() {
     return 'Hola, mi nombre es ' + this.nombre + ' y tengo ' + this.edad + ' años.'
   }
@@ -22,7 +23,51 @@ class Persona {
     return 'Estudio en la universidad ' + this.universidad;
   }
 
+  //funciones que modifican a la base
+  nuevo_saludo(){
+    this.nombre = prompt ('ingrese un nuevo nombre')
+    return 'Hola, yo soy ' + this.nombre;
+  }
 
+  nueva_edad(){
+    this.edad = prompt ('ingrese su nueva edad')
+    return 'Ahora tengo ' + this.edad + ' años ';
+  }
+
+  nuevo_estudio(){
+    this.carrera = prompt ('ingrese una nueva carrera')
+    return 'Ahora estudio ' + this.carrera;
+  }
+
+  nueva_universidad(){
+    this.universidad = prompt ('ingrese una nueva universidad')
+    return 'Ahora estudio en la Universidad' + this.universidad;
+  }
+
+  //funciones que eliminan a la base, devuelven un vacio
+  sin_nombre(){
+    alert ('su nombre ha sido eliminado')
+    this.nombre = ""
+    return this.nombre
+  }
+
+  sin_edad(){
+    alert ('su edad ha sido eliminada')
+    this.edad = ""
+    return this.edad
+  }
+
+  sin_carrera(){
+    alert ('su carrera ha sido eliminada')
+    this.carrera = ""
+    return this.carrera
+  }
+
+  sin_educacion(){
+    alert ('su nombre ha sido eliminado')
+    this.nombre = ""
+    return this.nombre
+  }
 }
 
 // Crear una instancia de la clase Persona
