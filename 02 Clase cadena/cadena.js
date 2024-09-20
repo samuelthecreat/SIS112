9d9b23442f5354ebd13f42d043e20974cc2fdab6
samuelthecreat <samuel.aguilera@ucb.edu.bo>
2024-09-20 09:56:32 -0400

    cadena

--- 02 Clase cadena/cadena.js
+++ 02 Clase cadena/cadena.js
@@ -47,3 +47,58 @@ class Cadena{
         return this.cadena.toLowerCase();
     }
 }
+
+let cadenaObj = new Cadena('');
+
+function mostrarCadena() {
+    document.getElementById('resultado').innerText = "Cadena: " + cadenaObj.getCadena();
+}
+
+function concatenarCadena() {
+    let nuevaCadena = prompt("Ingresa una cadena para concatenar:");
+    cadenaObj.cancatenar(nuevaCadena);
+    mostrarCadena();
+}
+
+function invertirCadena() {
+    cadenaObj.invertir();
+    mostrarCadena();
+}
+
+function verificarVacia() {
+    let esVacia = cadenaObj.esVacia() ? "Sí" : "No";
+    document.getElementById('resultado').innerText = "¿Es vacía?: " + esVacia;
+}
+
+function verificarContiene() {
+    let subcadena = prompt("Ingresa una subcadena para buscar:");
+    let contiene = cadenaObj.contiene(subcadena) ? "Sí" : "No";
+    document.getElementById('resultado').innerText = "Contiene '" + subcadena + "': " + contiene;
+}
+
+function comenzarCon() {
+    let prefijo = prompt("Ingresa un prefijo:");
+    let comienza = cadenaObj.comienzaCon(prefijo) ? "Sí" : "No";
+    document.getElementById('resultado').innerText = "Comienza con '" + prefijo + "': " + comienza;
+}
+
+function terminarCon() {
+    let sufijo = prompt("Ingresa un sufijo:");
+    let termina = cadenaObj.terminaCon(sufijo) ? "Sí" : "No";
+    document.getElementById('resultado').innerText = "Termina con '" + sufijo + "': " + termina;
+}
+
+function obtenerLongitud() {
+    let longitud = cadenaObj.longitud();
+    document.getElementById('resultado').innerText = "Longitud: " + longitud;
+}
+
+function convertirMayusculas() {
+    let mayusculas = cadenaObj.aMayusculas();
+    document.getElementById('resultado').innerText = "En mayúsculas: " + mayusculas;
+}
+
+function convertirMinusculas() {
+    let minusculas = cadenaObj.aMinusculas();
+    document.getElementById('resultado').innerText = "En minúsculas: " + minusculas;
+}
