c383d3506f6e9a8f68ef25145561a852e52c8080
samuelthecreat <samuel.aguilera@ucb.edu.bo>
2024-09-27 11:33:33 -0400

    entero

--- 03 Clase Lista Enteros/lista-enteros.js
+++ 03 Clase Lista Enteros/lista-enteros.js
@@ -1,10 +1,22 @@
 class listaEnteros {
-    constructor(){
+    constructor() {
         this.lista = [];
     }
 
-    agregar(valor){
+    agregar(valor) {
         this.lista.push(valor);
+    }
+    elimiar(valor) {
+        const index = this.lista.indexOf(valor);
+        if (index !== -1) {
+            this.lista.splice(index, 1);
+        }
+    }
+    buscar(valor) {
+        return this.lista.indexOf(valor);
+    }
+    ordenar() {
+        this.lista.sort((a, b) => a - b);
     }
 }
 
@@ -20,5 +32,27 @@ function agregarEntero() {
         document.getElementById('valor').value = ''; // Limpiar el campo
     } else {
         alert('Por favor, ingrese un número válido.');
+    }
+    // Función para buscar un entero en la lista y mostrar el resultado
+    function buscarEntero() {
+        const valor = parseInt(document.getElementById('valor').value);
+        if (!isNaN(valor)) {
+            const index = miLista.buscar(valor);
+            if (index !== -1) {
+                document.getElementById('mensaje').innerHTML = valor + ' se encuentra en la posición: ' + index;
+            } else {
+                document.getElementById('mensaje').innerHTML = valor + ' no se encuentra en la lista.';
+            }
+            document.getElementById('valor').value = ''; // Limpiar el campo
+        } else {
+            alert('Por favor, ingrese un número válido.');
+        }
+    }
+
+    // Función para ordenar la lista y mostrarla
+    function ordenarLista() {
+        miLista.ordenar();
+        document.getElementById('resultado').innerHTML = 'Lista: ' + miLista.lista.join(', ');
+        document.getElementById('mensaje').innerHTML = 'Lista ordenada.';
     }
 }
--- 03 Clase Lista Enteros/lista-enteros.py
+++ 03 Clase Lista Enteros/lista-enteros.py
@@ -4,7 +4,20 @@ class ListaEnteros:
         
     def agregar(self,valor):
         self.lista.append(valor)
-
+        
+    def eliminar_por_indice(self, indice):
+        if 0 <= indice < len(self.lista):
+            del self.lista[indice]
+            
+    def buscar(self, indice):
+        try:
+            return self.lista.index(valor)
+        except ValueError:
+            return -1
+    
+    def ordenar (self):
+        self.lista.sort()
+        
 mi_lista = ListaEnteros()
 mi_lista.agregar(5)
 mi_lista.agregar(2)
