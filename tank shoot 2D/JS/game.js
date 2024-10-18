4bead6da13186b594bc5a81c20fb5e977ec42ba6
samuelthecreat <samuel.aguilera@ucb.edu.bo>
2024-10-18 09:39:41 -0400

    gamejs

---
+++ tank shoot 2D/JS/game.js
@@ -0,0 +1,19 @@
+class Game{ 
+    ancho; 
+    alto; 
+    estadoJuego;
+
+    constructor (_ancho, _alto, _estadoJuego){
+        this.ancho = _ancho; 
+        this.alto = _ancho; 
+        this.estadoJuego = _estadoJuego;
+    }
+
+    start (){
+
+    }
+
+    reset(){
+        
+    }
+}
