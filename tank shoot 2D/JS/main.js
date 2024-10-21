862ada09d094688f28b8e945ceb7fc3c0759480b
samuelthecreat <samuel.aguilera@ucb.edu.bo>
2024-10-21 11:43:06 -0400

    js

--- tank shoot 2D/JS/main.js
+++ tank shoot 2D/JS/main.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById ("gameCanvas");
 const ctx = canvas.getContetx("2d"); 
 
 function resizeCanvas(){
-    convas.width = window.innerWidth *0.9; 
+    canvas.width = window.innerWidth * 0.9; 
     canvas.height = window.innerHeight * 0.9; 
 
 }
