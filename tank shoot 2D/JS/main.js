b418a50b35eca81b00559919ae742d35cc65af1b
samuelthecreat <samuel.aguilera@ucb.edu.bo>
2024-10-21 12:24:31 -0400

    mainjs

--- tank shoot 2D/JS/main.js
+++ tank shoot 2D/JS/main.js
@@ -10,3 +10,30 @@ function resizeCanvas(){
 resizeCanvas(); 
 
 window.addEventListener('resize', resizeCanvas);
+
+const game = new Game (canvas.width, canvas.height,'start'); 
+
+const playerTank = new tank (100, 100, 'up', 3); 
+const enemyTank = new Enemytank (500, 100, 'down', 3);
+
+function drawTank (tank) {
+    ctx.fillstyle = 'green'; 
+    ctx.fillRect(tank.posX, tank.posY, 50, 50); 
+}
+
+function drawEnemyTank (enemyTank){
+    ctx.fillstyle = 'red'; 
+    ctx.fillRect (enemyTank.posX, enemyTank.posY, 50,50);
+}
+
+function updateGame(){
+    ctx.clearRect(0,0, canvas.width, canvas.height); 
+
+    drawTank(playerTank); 
+    drawEnemyTank (enemyTank); 
+
+    requestAnimationFrame(updateGame); 
+
+}
+
+updateGame(); 
